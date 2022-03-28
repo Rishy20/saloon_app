0027b49b7205acdbaecc347ea3fcc955938689b9
Piumi <piumiekanayaka4@gmail.com>
2022-03-28 22:22:37 +0530

    Create models in hairstyles

---
+++ lib/models/hairStyles.dart
@@ -0,0 +1,44 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class HairStyles {
+    static const STYLE = "style";
+    static const IMAGE = "image";
+    static const DESCRIPTION = "description";
+    static const PRICE = "price";
+
+  late String _style;
+  late String _image;
+  late String _description;
+  late String _price;
+
+  set style(String value) {
+    _style = value;
+  } 
+   set image(String value) {
+    _image = value;
+  } 
+   set description(String value) {
+    _description = value;
+  } 
+  set price(String value) {
+    _price = value;
+  } 
+ 
+
+  String get style => _style;
+  String get image => _image;
+  String get description => _description;
+  String get price => _price;
+  
+  HairStyles();
+
+  Specialist.fromSnapshot(DocumentSnapshot snapshot) {
+    Map data = snapshot.data() as Map;
+    _style = data[STYLE];
+    _image = data[IMAGE];
+    _description = data[DESCRIPTION];
+    _price = data[PRICE];
+
+    
+  }
+}
